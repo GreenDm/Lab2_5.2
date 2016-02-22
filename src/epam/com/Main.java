7bb5e3fcbc21fc070d711b14b744dc97d1d2597c
Green <dr@mail.ru>
2016-02-22 20:48:35 +0200

    mass

---
+++ src/epam/com/Main.java
@@ -0,0 +1,15 @@
+package epam.com;
+
+/**
+ * Created by m18 on 22.02.2016.
+ */
+public class Main {
+    public static void main(String[] args) {
+
+        MyInitTast m1=new MyInitTast();
+        MyInitTast m2=new MyInitTast();
+
+        m1.printArray();
+        m2.printArray();
+    }
+}
